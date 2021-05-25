101f4631c397247c6f365e3f332c5f3cb345d747
Chris Sturtevant <sturtevant.chris@gmail.com>
2021-05-25 10:05:40 -0700

    Remove old code

--- src/main/java/edu/template/App.java
+++ src/main/java/edu/template/App.java
@@ -1,15 +1,10 @@
 package edu.template;
 
-import edu.template.server.Server;
-import java.io.IOException;
 
 public class App {
 
-    private static final int PORT = 8124;
 
-    // FIXME :: TK :: Need to handle this exception
-    public static void main( String[] args ) throws IOException {
-        Server server = new Server();
-        server.start(PORT);
+    public static void main( String[] args ) {
+        System.out.println("Working!");
     }
 }
